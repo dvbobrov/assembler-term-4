b000c6296b65a89455aa03ff9dc12b2e88aced6e
Dmitry Bobrov <me@dbobrov.com>
2013-04-28 23:13:18 +0400

    fat commit. almost ready

--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@ extern printf
 
 section .bss
 dct_matrix resd 64
-dct_matrix_transposed 64
+dct_matrix_transposed resd 64
 
 section .rodata
 
@@ -15,19 +15,183 @@ initialized db 0
 
 section .text
 
-global dct8x8
-global idct8x8
+global fdct
+global idct
 global calculate_dct_matrix
 global print_dct_matrix
 
 
-dct8x8: 
+fdct: ; (float * in, float * out, int n) {
+    call calculate_dct_matrix
+
+    push ebx
+    push esi
+    push edi
+    push ebp
+    mov ebp, esp
+    mov esi, [ebp + 20]
+    mov edi, [ebp + 24]
+    mov ebx, [ebp + 28]
+
+    sub esp, 12
+
+    mov [esp], dword dct_matrix
+
+    .loop:
+        mov [esp + 4], esi
+        mov [esp + 8], edi
+        call dct_impl
+        add esi, 64 * 4
+        add edi, 64 * 4
+        
+        dec ebx
+        test ebx, ebx
+        jnz .loop
+
+    mov esp, ebp
+    pop ebp
+    pop edi
+    pop esi
+    pop ebx
+    ret
+; }
+
+idct: ; (float * in, float * out, int n) {
+    call calculate_dct_matrix
+
+    push ebx
+    push esi
+    push edi
+    push ebp
+    mov ebp, esp
+    mov esi, [ebp + 20]
+    mov edi, [ebp + 24]
+    mov ebx, [ebp + 28]
+
+    sub esp, 12
+
+    mov [esp], dword dct_matrix_transposed
+
+    .loop:
+        mov [esp + 4], esi
+        mov [esp + 8], edi
+        call dct_impl
+        add esi, 64 * 4
+        add edi, 64 * 4
+
+        dec ebx
+        test ebx, ebx
+        jnz .loop
+
+    mov esp, ebp
+    pop ebp
+    pop edi
+    pop esi
+    pop ebx
+    ret
+; }
 
 
+dct_impl: ; (float * t, float * data, float * out) {
+    push ebx
+    push esi
+    push edi
+    push ebp
+    mov ebp, esp
 
-idct8x8:
+    and esp, -16
+    sub esp, 64 * 4
+    mov edi, esp
+    mov esi, [ebp + 24]
+    mov ebx, [ebp + 20]
 
-print_dct_matrix:
+    xor eax, eax
+    .loop1:
+        cmp eax, 8
+        je .endloop1
+
+        mov ebx, [ebp + 20]
+        xor ecx, ecx
+        .loop2:
+            cmp ecx, 8
+            je .endloop2
+
+            movaps xmm0, [ebx]
+            mulps xmm0, [esi]
+
+            movapd xmm1, [ebx + 16]
+            mulps xmm1, [esi + 16]
+
+            addps xmm0, xmm1
+            
+            haddps xmm0, xmm0
+            haddps xmm0, xmm0
+
+            lea edx, [eax + ecx * 8]
+            movd [edi + edx * 4], xmm0
+
+            add ebx, 32
+
+            inc ecx
+            jmp .loop2
+
+        .endloop2:
+        add esi, 32
+        inc eax
+        jmp .loop1
+
+    .endloop1:
+
+    mov esi, esp
+    mov edi, [ebp + 28]
+    mov ebx, [ebp + 20]
+
+    xor eax, eax
+    .loop3:
+        cmp eax, 8
+        je .endloop3
+
+        mov esi, esp
+        xor ecx, ecx
+        .loop4:
+            cmp ecx, 8
+            je .endloop4
+
+            movaps xmm0, [ebx]
+            mulps xmm0, [esi]
+
+            movapd xmm1, [ebx + 16]
+            mulps xmm1, [esi + 16]
+
+            addps xmm0, xmm1
+            
+            haddps xmm0, xmm0
+            haddps xmm0, xmm0
+
+            lea edx, [ecx + eax * 8]
+            movd [edi + edx * 4], xmm0
+
+            add esi, 32
+
+            inc ecx
+            jmp .loop4
+
+        .endloop4:
+        add ebx, 32
+        inc eax
+        jmp .loop3
+
+    .endloop3:
+
+    mov esp, ebp
+    pop ebp
+    pop edi
+    pop esi
+    pop ebx
+    ret
+; }
+
+print_dct_matrix: ; () {
     push ebx
     xor ebx, ebx
     .loop3:
@@ -44,9 +208,9 @@ print_dct_matrix:
     .endloop3:
     pop ebx
     ret
+; }
 
-
-calculate_dct_matrix:
+calculate_dct_matrix: ; () {
    push ebx
    mov ebx, [initialized]
    test ebx, ebx
@@ -73,14 +237,16 @@ calculate_dct_matrix:
         jmp .loop1
     .endloop1:
 
-    mov [initialized], 1
+    mov [initialized], byte 1
     .finish:
         pop ebx
         ret
 
-calculate_dct_element: ; i = eax; j = ebx
+; }
+
+calculate_dct_element: ; (eax &, ebx &) {
     test eax, eax
-    jz dct_el_zero
+    jz .dct_el_zero
 
     push eax
 
@@ -111,13 +277,13 @@ calculate_dct_element: ; i = eax; j = ebx
     fstp dword [dct_matrix_transposed + ecx * 4]
 
     ret
-    
 
-dct_el_zero:
-    push dword 0.125
-    fld dword [esp]
-    add esp, 4
-    fsqrt
-    lea ecx, [eax * 8 + ebx]
-    fstp dword [dct_matrix + ecx * 4]
-    ret
+    .dct_el_zero:
+        push dword 0.125
+        fld dword [esp]
+        add esp, 4
+        fsqrt
+        lea ecx, [eax * 8 + ebx]
+        fstp dword [dct_matrix + ecx * 4]
+        ret
+; }
--- test.cpp
+++ test.cpp
@@ -2,14 +2,44 @@
 
 extern "C" {
     void calculate_dct_matrix();
-    void dct8x8(float * in, float * out, int n);
-    void idct8x8(float * in, float * out, int n);
+    void fdct(float * in, float * out, int n);
+    void idct(float * in, float * out, int n);
     void print_dct_matrix();
 }
 
+float test[64];
+float res[64];
+
 int main()
 {
-    calculate_dct_matrix();
-    print_dct_matrix();
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            test[i * 8 + j] = j;
+        }
+    }
+
+    fdct(test, res, 1);
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            std::cout << res[i * 8 + j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "---\n\n";
+
+    idct(res, test, 1);
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            std::cout << test[i * 8 + j] << " ";
+        }
+        std::cout << "\n";
+    }
 }
 
